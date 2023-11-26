14580639a35ebb3177e71cd882ad03e2cf0a0af5
rohitpatil597 <30457945+rohitpatil597@users.noreply.github.com>
2023-11-26 15:16:02 +0530

    multi_divide_2.c

---
+++ multi_divide_2.c
@@ -0,0 +1,12 @@
+/*Divide and multiply by 2 */
+
+#include <stdio.h>
+
+int main()
+{
+    int a=10;
+    int Result=a<<1;
+    int result_1=a>>1;
+    printf("Value of Multipication result=%d\nand Divide= %d",Result,result_1);
+    return 0;
+}
