d0948607321b73af510195f8d419519e633d8eff
rohitpatil597 <30457945+rohitpatil597@users.noreply.github.com>
2023-11-28 18:09:45 +0530

    Clear_MSB.c

---
+++ Bitwise Operator/Clear_MSB.c
@@ -0,0 +1,19 @@
+/*Clear MSB to ith Poisition */
+
+#include <stdio.h>
+#include <inttypes.h>
+
+int main()
+{
+    uint32_t data=15;
+    uint8_t i=2;
+    uint32_t mask=0;
+    
+    mask=(1 << (i+1));
+    mask=mask-1;
+    
+    data=data&mask;
+    
+    printf("0x%x",data);
+    return 0;
+}
