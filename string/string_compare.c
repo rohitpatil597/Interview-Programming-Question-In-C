61301aaa9981f87515094ceb391442a067a280fd
rohitpatil597 <30457945+rohitpatil597@users.noreply.github.com>
2023-11-30 17:29:27 +0530

    string_compare.c

--- string/string_compare.c
+++ string/string_compare.c
@@ -1,11 +1,22 @@
+/*Compare two string */
 #include <stdio.h>
 #include <string.h>
 
-void User_Str_Cmp(const char * pBuffer1, const char * pBuffer2, const int length)
+const int len(const char* str)
+{
+    int i;
+    for(i=0;str[i] !='\0';i++)
+    {
+        
+    }
+    //printf("\n%d",i);
+    return i;
+}
+void User_Str_Cmp(const char * pBuffer1, const char * pBuffer2)
 {
     int idx = 0;
     int count=0;
-    for (idx = 0; idx < strlen(pBuffer1); idx++)
+    for (idx = 0; idx < len(pBuffer1); idx++)
     {
         if(pBuffer1[idx] != pBuffer2[idx])
         {
@@ -21,6 +32,7 @@ void User_Str_Cmp(const char * pBuffer1, const char * pBuffer2, const int length
 
 int main()
 {
-User_Str_Cmp("Rohit","RoHit",5);
+User_Str_Cmp("Rohit","RoHit");
+len("Rohit");
 return 0;
 }
